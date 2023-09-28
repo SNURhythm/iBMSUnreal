699196952425881f10a6fdd715a6279178f10f0a
VioletXF <zudevxf@gmail.com>
2023-09-28 20:27:05 +0900

    Reformat indent & Refresh list on load end

--- Source/iBMSUnreal/Private/BMSGameModeBase.cpp
+++ Source/iBMSUnreal/Private/BMSGameModeBase.cpp
@@ -89,118 +89,119 @@ void ABMSGameModeBase::InitGame(const FString& MapName, const FString& Options, 
 
 void ABMSGameModeBase::LoadCharts()
 {
-	    FTask LoadTask = Launch(UE_SOURCE_LOCATION, [&]() {
-        FMOD::Sound* SuccessSound;
-        const FString SoundPathRel = FPaths::Combine(FPaths::ProjectContentDir(), "Sounds/success.wav");
-        const FString SoundPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*SoundPathRel);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase SoundPath: %s"), *SoundPath);
-        // /private/var/containers/Bundle/Application/GUID/iBMSUnreal.app/Documents
-        const FString DocumentsPathRel = FPaths::RootDir();
-        const FString DocumentsPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*DocumentsPathRel);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase DocumentsPath: %s"), *DocumentsPath);
+	    FTask LoadTask = Launch(UE_SOURCE_LOCATION, [&]()
+	    {
+		    FMOD::Sound* SuccessSound;
+			const FString SoundPathRel = FPaths::Combine(FPaths::ProjectContentDir(), "Sounds/success.wav");
+			const FString SoundPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*SoundPathRel);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase SoundPath: %s"), *SoundPath);
+			// /private/var/containers/Bundle/Application/GUID/iBMSUnreal.app/Documents
+			const FString DocumentsPathRel = FPaths::RootDir();
+			const FString DocumentsPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*DocumentsPathRel);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase DocumentsPath: %s"), *DocumentsPath);
 
-        FMODSystem->createSound(TCHAR_TO_ANSI(*SoundPath), FMOD_DEFAULT, 0, &SuccessSound);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase Start Task!!"));
-        auto dbHelper = ChartDBHelper::GetInstance();
-        auto db = dbHelper.Connect();
-        dbHelper.CreateTable(db);
-        auto chartMetas = dbHelper.SelectAll(db);
-	    	AsyncTask(ENamedThreads::GameThread, [&]()
-	    	{
-	    		ChartSelectUI->ChartList->SetListItems(chartMetas);
-	    	});
+			FMODSystem->createSound(TCHAR_TO_ANSI(*SoundPath), FMOD_DEFAULT, 0, &SuccessSound);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase Start Task!!"));
+			auto dbHelper = ChartDBHelper::GetInstance();
+			auto db = dbHelper.Connect();
+			dbHelper.CreateTable(db);
+			auto chartMetas = dbHelper.SelectAll(db);
+				AsyncTask(ENamedThreads::GameThread, [&]()
+				{
+					ChartSelectUI->ChartList->SetListItems(chartMetas);
+				});
 	    	
-        // find new charts
-        TArray<FDiff> Diffs;
-        TSet<FString> PathSet;
-        std::atomic_int SuccessCount;
-        // TODO: Initialize prevPathSet by db
-        for (auto& meta : chartMetas) {
-			if (bCancelled) break;
-            auto& path = meta->BmsPath;
-			PathSet.Add(path);
-		}
-        IFileManager& FileManager = IFileManager::Get();
-        // use iOS Document Directory
-#if PLATFORM_IOS
-        // mkdir "BMS"
-        FString DirectoryRel = FPaths::Combine(FPaths::RootDir(), "BMS/");
-        FileManager.MakeDirectory(*DirectoryRel);
-#else
-        // use Project/BMS. Note that this would not work on packaged build, so we need to make it configurable
-        FString DirectoryRel = FPaths::Combine(FPaths::ProjectDir(), "BMS/");
-#endif
-        FString Directory = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*DirectoryRel);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase Directory: %s"), *Directory);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase FindNew"));
-        // print bCancelled
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase isCancelled: %d"), (bool)bCancelled);
-        FindNew(Diffs, PathSet, Directory, bCancelled);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase FindNew Done: %d"), Diffs.Num());
-        // find deleted charts
-        for (auto& path : PathSet) {
-            if (bCancelled) break;
-            if (!FileManager.FileExists(*path)) {
-                auto diff = FDiff();
-                diff.path = path;
-                diff.type = EDiffType::Deleted;
-                Diffs.Add(diff);
-            }
-        }
+			// find new charts
+			TArray<FDiff> Diffs;
+			TSet<FString> PathSet;
+			std::atomic_int SuccessCount;
+			// TODO: Initialize prevPathSet by db
+			for (auto& meta : chartMetas) {
+				if (bCancelled) break;
+				auto& path = meta->BmsPath;
+				PathSet.Add(path);
+			}
+			IFileManager& FileManager = IFileManager::Get();
+			// use iOS Document Directory
+	#if PLATFORM_IOS
+			// mkdir "BMS"
+			FString DirectoryRel = FPaths::Combine(FPaths::RootDir(), "BMS/");
+			FileManager.MakeDirectory(*DirectoryRel);
+	#else
+			// use Project/BMS. Note that this would not work on packaged build, so we need to make it configurable
+			FString DirectoryRel = FPaths::Combine(FPaths::ProjectDir(), "BMS/");
+	#endif
+			FString Directory = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*DirectoryRel);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase Directory: %s"), *Directory);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase FindNew"));
+			// print bCancelled
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase isCancelled: %d"), (bool)bCancelled);
+			FindNew(Diffs, PathSet, Directory, bCancelled);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase FindNew Done: %d"), Diffs.Num());
+			// find deleted charts
+			for (auto& path : PathSet) {
+				if (bCancelled) break;
+				if (!FileManager.FileExists(*path)) {
+					auto diff = FDiff();
+					diff.path = path;
+					diff.type = EDiffType::Deleted;
+					Diffs.Add(diff);
+				}
+			}
 
-        if (Diffs.Num() > 0) {
-	        const bool bSupportMultithreading = FPlatformProcess::SupportsMultithreading();
-            const int TaskNum = FPlatformMisc::NumberOfWorkerThreadsToSpawn() - 1;
-            UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase taskNum: %d"), TaskNum);
-            const int TaskSize = Diffs.Num() / TaskNum;
-            dbHelper.BeginTransaction(db);
-            std::atomic_bool isCommitting;
-            ParallelFor(TaskNum, [&](int32 idx) {
-                if (bCancelled) return;
-                const int Start = idx * TaskSize;
-                if (Start >= Diffs.Num()) return;
-                int end = (idx + 1) * TaskSize;
-                if (idx == TaskNum - 1) end = Diffs.Num();
-                if (end > Diffs.Num()) end = Diffs.Num();
-                for (int i = Start; i < end; i++) {
-                    if (bCancelled) return;
-                    auto& diff = Diffs[i];
+			if (Diffs.Num() > 0)
+			{
+				const bool bSupportMultithreading = FPlatformProcess::SupportsMultithreading();
+				const int TaskNum = FPlatformMisc::NumberOfWorkerThreadsToSpawn() - 1;
+				UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase taskNum: %d"), TaskNum);
+				const int TaskSize = Diffs.Num() / TaskNum;
+				dbHelper.BeginTransaction(db);
+				std::atomic_bool isCommitting;
+				ParallelFor(TaskNum, [&](int32 idx) {
+					if (bCancelled) return;
+					const int Start = idx * TaskSize;
+					if (Start >= Diffs.Num()) return;
+					int end = (idx + 1) * TaskSize;
+					if (idx == TaskNum - 1) end = Diffs.Num();
+					if (end > Diffs.Num()) end = Diffs.Num();
+					for (int i = Start; i < end; i++) {
+						if (bCancelled) return;
+						auto& diff = Diffs[i];
 
-                    if (diff.type == EDiffType::Added)
-                    {
-	                    const auto Parser = new FBMSParser();
-                        FChart* Chart;
-                        Parser->Parse(diff.path, &Chart, false, false);
-                        ++SuccessCount;
-                        if (SuccessCount % 100 == 0) {
-                            UE_LOG(LogTemp, Warning, TEXT("success count: %d"), (int)SuccessCount);
-                            if (isCommitting) continue;
-                            isCommitting = true;
-                            dbHelper.CommitTransaction(db);
-                            dbHelper.BeginTransaction(db);
-                            isCommitting = false;
+						if (diff.type == EDiffType::Added)
+						{
+							const auto Parser = new FBMSParser();
+							FChart* Chart;
+							Parser->Parse(diff.path, &Chart, false, false);
+							++SuccessCount;
+							if (SuccessCount % 100 == 0) {
+								UE_LOG(LogTemp, Warning, TEXT("success count: %d"), (int)SuccessCount);
+								if (isCommitting) continue;
+								isCommitting = true;
+								dbHelper.CommitTransaction(db);
+								dbHelper.BeginTransaction(db);
+								isCommitting = false;
 
-                        }
+							}
                             
-                        UE_LOG(LogTemp,Warning,TEXT("TITLE: %s"), *Chart->Meta->Title);
+							UE_LOG(LogTemp,Warning,TEXT("TITLE: %s"), *Chart->Meta->Title);
 
-                        dbHelper.Insert(db, *Chart->Meta);
-                        // close db
-                        delete Chart;
-                        delete Parser;
-                    }
-                }
+							dbHelper.Insert(db, *Chart->Meta);
+							// close db
+							delete Chart;
+							delete Parser;
+						}
+					}
 
-                }, !bSupportMultithreading);
-            dbHelper.CommitTransaction(db);
-        }
-        const auto Result = FMODSystem->playSound(SuccessSound, nullptr, false, nullptr);
-        UE_LOG(LogTemp, Warning, TEXT("FMODSystem->playSound: %d"), Result);
-        UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase End Task"));
-        UE_LOG(LogTemp, Warning, TEXT("success count: %d"), (int)SuccessCount);
-
-
-        });
+					}, !bSupportMultithreading);
+				dbHelper.CommitTransaction(db);
+			}
+				ChartSelectUI->ChartList->SetListItems(dbHelper.SelectAll(db));
+			const auto Result = FMODSystem->playSound(SuccessSound, nullptr, false, nullptr);
+			UE_LOG(LogTemp, Warning, TEXT("FMODSystem->playSound: %d"), Result);
+			UE_LOG(LogTemp, Warning, TEXT("BMSGameModeBase End Task"));
+			UE_LOG(LogTemp, Warning, TEXT("success count: %d"), (int)SuccessCount);
+	    });
 }
 void ABMSGameModeBase::BeginPlay()
 {
