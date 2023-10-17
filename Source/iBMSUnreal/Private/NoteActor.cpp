dc252a3b3e80de247c6214e4d61d26c373df809e
VioletXF <zudevxf@gmail.com>
2023-10-17 23:24:40 +0900

    Disable actor tick

--- Source/iBMSUnreal/Private/NoteActor.cpp
+++ Source/iBMSUnreal/Private/NoteActor.cpp
@@ -7,7 +7,7 @@
 ANoteActor::ANoteActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	// init root component
 	// RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RenderComponent"));
 	// RootComponent->SetMobility(EComponentMobility::Movable);
