d9ec7f13235bf4fb59a2631bfd7bb73adc221410
VioletXF <zudevxf@gmail.com>
2023-11-23 05:30:30 +0900

    Fix include order

--- Source/iBMSUnreal/Private/ChartDBHelper.cpp
+++ Source/iBMSUnreal/Private/ChartDBHelper.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "iOSNatives.h"
 #include "ChartDBHelper.h"
+#include "iOSNatives.h"
+
 
 
 sqlite3* ChartDBHelper::Connect() {
